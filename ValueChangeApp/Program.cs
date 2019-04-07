25e713f7b7ae3cde15747046048f827658bce673
unala <unalavci@yandex.com>
2019-04-08 00:01:19 +0300

    new file:   ValueChangeApp.sln 	new file:   ValueChangeApp/App.config 	new file:   ValueChangeApp/Program.cs 	new file:   ValueChangeApp/Properties/AssemblyInfo.cs 	new file:   ValueChangeApp/ValueChangeApp.csproj 	new file:   ValueChangeApp/bin/Debug/ValueChangeApp.exe 	new file:   ValueChangeApp/bin/Debug/ValueChangeApp.exe.config 	new file:   ValueChangeApp/bin/Debug/ValueChangeApp.pdb 	new file:   ValueChangeApp/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   ValueChangeApp/obj/Debug/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs 	new file:   ValueChangeApp/obj/Debug/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs 	new file:   ValueChangeApp/obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs 	new file:   ValueChangeApp/obj/Debug/ValueChangeApp.csproj.CoreCompileInputs.cache 	new file:   ValueChangeApp/obj/Debug/ValueChangeApp.csproj.FileListAbsolute.txt 	new file:   ValueChangeApp/obj/Debug/ValueChangeApp.csprojAssemblyReference.cache 	new file:   ValueChangeApp/obj/Debug/ValueChangeApp.exe 	new file:   ValueChangeApp/obj/Debug/ValueChangeApp.pdb

---
+++ ValueChangeApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueChangeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process();
+        }
+
+        public static void Process()
+        {
+            int number1 = 10;
+            int number2 = 20;
+
+            Console.WriteLine("Initial values");
+            Console.WriteLine("Number 1={0}",number1);
+            Console.WriteLine("Number 2={0}",number2);
+            Console.WriteLine("______________________________");
+
+
+
+            number1 = number1 + number2;
+            number2 = number1 - number2;
+
+            number1 = number1 - number2;
+
+            Console.WriteLine("After changing the values");
+            Console.WriteLine("Number 1={0}", number1);
+            Console.WriteLine("Number 2={0}", number2);
+            Console.ReadKey();
+        }
+    }
+}
